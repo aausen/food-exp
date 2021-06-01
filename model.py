19b9c81beaaf3056f4c6f3142e38d318552ad7d0
Annie Ausen <ausen.annie@gmail.com>
2021-06-01 12:03:32 -0500

    work on model.py

--- model.py
+++ model.py
@@ -72,3 +72,20 @@ class User_food(db.Model):
 
     def __repr__(self):
         return f"<User_food user_food_id = {self.user_food_id} food_id = {self.food_id} user_id = {self.user_id}>"
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///food_exp', echo = True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ = '__main__':
+    from server import app
+
+    connect_to_db(app)
