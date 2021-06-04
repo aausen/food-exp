6f9ff6a0e4848454738a40c05a14af21bee626dc
Annie Ausen <ausen.annie@gmail.com>
2021-06-04 11:59:48 -0500

    finish login/logout

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for food expiration app"""
 
 from flask import (Flask, render_template, request, flash, session, redirect)
-from flask_login import LoginManager, login_user, login_required
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from jinja2.runtime import StrictUndefined
 from model import connect_to_db, User
 import crud
@@ -17,19 +17,25 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
+    
     return User.query.get(user_id)
 
 @app.route('/')
 def show_homepage():
     """View homepage"""
-    # if "user_email" in session:
-    return render_template("homepage.html")
+    if current_user.is_authenticated:
+        return render_template("homepage.html")
     
-    # else:
-    #     return redirect("/login")
+    else:
+        return redirect("/login")
+
+@app.route('/users', method=["GET"])
+def register_form():
+
+    return render_template('register.html')
 
 @app.route('/users', methods=["POST"])
-def register_user():
+def register_process():
     """Create a new user"""
 
     email = request.form.get("email")
@@ -38,11 +44,13 @@ def register_user():
 
     if user:
         flash("A user already exists with that email. Please use a different email.")
+
+        return redirect('/users')
     else:
         crud.create_user(email, password)
         flash("Account created! Please log in.")
 
-        return redirect('login.html')
+        return redirect('/login')
 
 @app.route("/login", methods=["GET"])
 def login_form():
@@ -70,17 +78,13 @@ def login_process():
         flash("Sorry, try again.")
         return redirect("/login")
 
-    # if not user or user.password != password:
-    #     flash("The email or password you entered are incorrect.")
-    #     return redirect("/login")
-    # else:
-    #     #Log in user by storing user's email in session
-    #     session["user_email"] = user.email
-    #     flash(f"Welcome back {user.email}!")
+@app.route("/logout")
+def logout():
+    """Process logout."""
 
-    #     return redirect("/")
+    logout_user()
 
-
+    return redirect('/login')
 
 if __name__ == '__main__':
     connect_to_db(app)
