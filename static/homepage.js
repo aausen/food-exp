f211ae08e2e35b10e393119e9d96590c16192a73
Annie Ausen <ausen.annie@gmail.com>
2021-06-07 15:05:47 -0500

    started js work on search bar

---
+++ static/homepage.js
@@ -0,0 +1,30 @@
+'use strict';
+const foodItem = document.getElementById('foodItem')
+const searchBar = document.getElementById('searchBar')
+
+searchBar.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+});
+
+// const loadFood = $.get('https://shelf-life-api.herokuapp.com/search?q=%7Bsearch_query%7D', (res) =>{
+//     let foodInfo = res;
+//     displayFood(foodInfo);
+//     console.log(foodInfo);
+// })
+
+// const displayFood = (food) =>{
+//     const htmlString = food
+//     .map((food) => {
+//         return `
+//         <li class="food">
+//             <h2>${food.name}</h2>
+//             <p>Location: ${food.location}</p>
+//             <p>Shelf life: ${food.expiration}</p>
+//             <p>Tips: ${food.tips}</p>
+//             </li>
+//             `;
+//     })
+//     foodItem.innerHTML = htmlString;
+// };
+
+// loadFood();
