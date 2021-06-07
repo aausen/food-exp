17bfcaa0a94ed1e4fc7987cc28130eb771f7419c
Annie Ausen <ausen.annie@gmail.com>
2021-06-07 11:58:58 -0500

    work on submit button functionality

--- server.py
+++ server.py
@@ -89,6 +89,11 @@ def logout():
 
     return redirect('/login')
 
+@app.route("/search")
+def food_search():
+    """Search the api for possible foods"""
+    input_food = request.args.get("search")
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug = True)
