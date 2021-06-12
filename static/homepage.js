ac5039a48713c2e8efb7b6896decca1faa5d3f6c
Annie Ausen <ausen.annie@gmail.com>
2021-06-12 11:02:46 -0500

    work on dynamically updating exp id

--- static/homepage.js
+++ static/homepage.js
@@ -1,10 +1,28 @@
 'use strict';
 
-const expDate = document.getElementById('exp_date').textContent;
-    console.log(expDate)
-    const timeNow = new Date();
-    if(expDate == timeNow){
-        expDate.style.color = 'red';
-    }
+// const expDate = document.getElementById('exp_date').value;
+//     console.log(expDate)
+//     const timeNow = new Date();
+//     if(expDate == timeNow){
+//         expDate.style.color = 'red';
+//     }
 
-console.log(new Date())
+// console.log(new Date())
+
+
+// let exp_date = document.getElementsByClassName('exp_date');
+// const table = document.getElementsByClassName('table');
+// tbody = table.tBodies[0].rows.length;
+
+// console.log(tbody);
+// console.log(exp_date.rows.length);
+// for (let i =0; i < exp_date.length; i++) {
+
+//     let table = tables[i];
+
+//     table.setAttribute("id", i + 1);
+
+// }
+
+let table = document.getElementsByClassName('exp_date').val();
+console.log(table);
