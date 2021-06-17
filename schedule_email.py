6c82c860c013b19ab946da2dde391e832b8014d7
Annie Ausen <ausen.annie@gmail.com>
2021-06-17 16:56:04 -0500

    added pseudocode for job funct

--- schedule_email.py
+++ schedule_email.py
@@ -7,11 +7,11 @@ os.system("source secrets.sh")
 API_KEY = os.environ['API_KEY']
 send_email = os.environ['send_email']
 
-def send_email():
+def send_email(user_email, food_name):
     message = Mail(
         from_email=send_email,
-        to_emails="devtest292@gmail.com",
-        subject="New email",
+        to_emails=user_email,
+        subject="Time to toss the {food_name}",
         html_content="<strong>and easy to do anywhere, even in Python</strong>")
     try:
         sg = SendGridAPIClient(API_KEY)
@@ -21,3 +21,10 @@ def send_email():
         print(response.headers)
     except Exception as e:
         print(e.message)
+
+def job():
+    # Get expiration dates from db
+    # find user attached to exp_date
+    # Get food name
+    # If exp_date = date_now:
+    #   send email
